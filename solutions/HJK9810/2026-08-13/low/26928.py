21d4db00d24ebc18a57259e36cde8353679f2929
김희주 <saiiytkfkd@naver.com>
2026-08-13 09:31:48 +0900

    solve: 202-08-13 SWEA low

---
+++ solutions/HJK9810/2026-08-13/low/26928.py
@@ -0,0 +1,32 @@
+def check_count(end, target):
+    left = 1
+    right = end
+    count = 0
+
+    while True:
+        center = (left + right) // 2
+        if center < target:
+            left = center
+        elif center > target:
+            right = center
+        else:
+            break
+
+        count += 1
+
+    return count
+
+def algorithm():
+    P, PA, PB = map(int, input().split())
+
+    A_count = check_count(P, PA)
+    B_count = check_count(P, PB)
+
+    if A_count == B_count: return 0
+    elif A_count > B_count: return 'B'
+    else: return 'A'
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    print(f"#{test_case} {algorithm()}")
